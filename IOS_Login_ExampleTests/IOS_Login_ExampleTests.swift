2657ec9f28da7425cf54ff2e4594b05bdd01f673
SunSet0418 <coffee@soylatte.kr>
2018-03-11 23:30:28 +0900

    Initial Commit

---
+++ IOS_Login_ExampleTests/IOS_Login_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  IOS_Login_ExampleTests.swift
+//  IOS_Login_ExampleTests
+//
+//  Created by SungWoon Cho on 2018. 3. 11..
+//  Copyright © 2018년 SunSet. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_Login_Example
+
+class IOS_Login_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
